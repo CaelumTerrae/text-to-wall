1c3d6626a5285bdaa2cf6459d2165c9add2ca7b2
CaelumTerrae <gavinlifrieri@gmail.com>
2020-08-08 18:35:05 -0700

    actually only need path for sms

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 currentURL = None
 
-@app.route("/mms", methods=['GET', 'POST'])
+@app.route("/sms", methods=['GET', 'POST'])
 def mms_reply():
     """Respond to incoming image texts"""
     imageURL = request.values.get('MediaUrl0', None)
@@ -18,13 +18,6 @@ def mms_reply():
     else:
         resp.message("Thank you for sending this image! It will promptly be displayed on Daran and Gavin's living room wall.")
         currentURL = imageURL
-    return str(resp)
-
-@app.route("/sms", methods=['GET', 'POST'])
-def sms_reply():
-    """Respond to incoming calls with a simple text message."""
-    resp = MessagingResponse()
-    resp.message("Please attach an image you want to see on Daran and Gavin's wall")
     return str(resp)
 
 @app.route("/wall", methods=['GET'])
